6302a0d3ed3ebce5d39b51e68ac957b3c4636076
unknown <arsalanwani44@gmail.com>
2026-08-12 11:44:59 +0530

    add python practice files

---
+++ practice.py
@@ -0,0 +1,281 @@
+# list stores multiple values in a single  variable its heterogenous(takes different data types in a list),it is ordered 
+# indexing is a numerical location of an item in a list.
+# it is mutable.
+
+
+
+# print(lt)
+# lt[1]='ayan' 
+
+# lt.append('faheem')
+# lt.insert(2,'seerat')
+# lt.remove('ishan')
+# lt.pop(0)
+# print(lt)
+
+# fruits = ["Apple","Mango","Banana","grapes","pear"]
+# print(fruits[1])
+# print(fruits[-1])
+# fruits[2]="kiwi"
+# print(fruits)
+# fruits.append("orange")
+# print(fruits)
+# fruits.remove("Apple")
+# print(fruits)
+# print(len(fruits))
+# fruits.append("kiwi")
+# print(fruits)
+# fruits.insert(2,"watermelon")
+# print(fruits)
+# fruits.insert(0,"cherry")
+# print(fruits)
+# fruits.insert(4,"papaya")
+# print(fruits)
+# fruits.remove("kiwi")
+# print(fruits)
+# fruits.remove("watermelon")
+# fruits.remove("pear")
+# print(fruits)
+# fruits.pop()
+# print(fruits)
+# fruits.pop(1)
+# print(fruits)
+# removed_fruit= fruits.pop(1)
+# print(removed_fruit)
+# fruits.append("mango")
+# print(fruits)
+# fruits.remove("mango")
+# print(fruits)
+# fruits.insert(3,"kiwi")
+# print(fruits)
+# fruits.pop()
+# print(fruits)
+# fruits.remove('cherry')
+# print(fruits)
+# fruits.append('strawberry')
+# print(fruits)
+# fruits.insert(0,'guava')
+# print(fruits)
+# fruits.remove('grapes')
+# print(fruits)
+
+
+
+
+# lt=[['arsalan','muzammil','ayat','ishan','sabtain',2,4,6,8],["Apple","Mango","Banana","grapes","pear"]]
+# lt[0].append('arsii')
+# print(lt)
+
+
+
+# lt=[['arsii','wasiq','wani'],[1,2,3],[True,False,9]]
+# lt[0].append('ayan')
+# lt[2].pop(2)
+# lt[0][2]=('athar')
+# print(lt)
+# print(lt[0][1])
+# lt = [["Ali","Sara"],["Apple","Mango"]]
+# print(lt[0][1])
+# lt[1].append('Banana')
+# lt[0][0]='Arsalan'
+# lt[1].pop(1)
+# print(lt)
+# lt = [
+#     ["Ali","Sara","John"],
+#     [10,20,30],
+#     [True,False]
+# ]
+# lt[1][1]=25
+# lt[1].append(40)
+# lt[2].pop(1)
+# print(lt[0][2])
+# print(lt[2][0])
+# # print(lt)
+# lt = [
+#     ['arsii','wasiq','wani'],
+#     [10,20,30],
+#     [True,False]
+# ]
+
+# [
+#  ['Arsalan','Wasiq','Athar','Ayan'],
+#  [10,20,30,40],
+#  [True]
+# ]
+# lt[0][0]='Arsalan'
+# lt[0][2]='Athar'
+# lt[0].append("Ayan")
+# lt[1].append(40)
+# lt[2].pop(1)
+# print(lt)+
+# lt=[[['arsii','athar','wasiq'],[12,13,True]],
+#     [['faheeem','ayan','iqra'],[31,76,False]],
+#     [['falak','iq','nocki'],['t24','zhuis','spower']]]
+# print(lt[2][0][0])
+# lt[1][1][0]='arsiii'
+# print(lt[1][1][0])
+# lt[1][1].insert(1,'athar')
+# lt[1][0].append('maryam')
+# del (lt[0][1][2])
+# print(lt[0])
+# print(lt[1])
+
+
+
+# dt={
+#     "Name":{
+#         "FirstName":"Arsalan",
+#         "LastName":"wani"
+#     },
+
+#     "Address":{
+#         "state":"kashmir",
+#         "pincode":"192121"
+#     }
+# }
+# dt["occuption"]= {"designation":"jr assistant","employ-code":"1903"}
+# dt["Address"]["pincode"]="192122"
+# del dt["Address"]["pincode"]
+# # print(dt)
+# print(dt["occuption"]["employ-code"])
+# print(dt["Address"]["state"])
+
+
+# student ={
+#     "name":{
+#         "firstname":"imaad",
+#         "lastname":"bhat"
+#     },
+#     "address":{
+#         "state":"kupwara",
+#         "pincode":"192121"
+#     },
+#     "education":{
+#         "course":"datascience",
+#         "year":"2026"
+#     }
+# }
+
+
+
+# students={
+#     "1":{
+#     "name":{
+#         "firstname":"imaad",
+#         "lastname":"bhat"
+#         },
+
+#     "address":{
+#         "state":"kashmir",
+#         "pincode":"192121"
+#     }
+#     },
+#     "2":{
+#         "name":{
+#             "firstname":"ubaid",
+#             "lastname":"khan"
+#         },
+#         "address":{
+#             "state":"kashmir",
+#             "pincode":"192121"
+#         }
+
+#     }
+#         }
+    
+# print(students)
+# print(students["1"]["address"]["pincode"])
+# print(students["2"]["name"]["lastname"])
+
+
+
+
+
+# grade = int(input("enter your number:-"))
+
+# if grade == 98:
+#     print("Topper")
+# elif grade >= 50:
+#     print("Good")
+# elif grade >= 30:
+#     print("Average")  
+# else:
+#     print("Fail");
+
+
+
+
+# age = int(input("enter your age :- "))
+
+
+# if age>=18:
+#     print("you are eligible to vote")
+# else:
+#     print("you are a minor👶👶")
+
+
+
+
+
+
+
+# x=int(input("enter 1st number "))
+# y=int(input("enter 2nd number "))
+# z=int(input("enter 3rd number "))
+
+# if x>y and x>z:
+#     print(x," is greater ")
+# elif y>x and y>z:
+#     print(y," is greater ") 
+# else:
+#     print(z," is greater ")   
+     
+
+# num = int(input("enter your number"))
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")    
+
+
+# num = int(input("enter your number "))
+# if num > 0:
+#     print("positive")
+# elif num ==0:
+#     print("neutral")    
+# else:
+#     print("negative")    
+
+
+
+
+# username = input("enter your username ")
+# password = input("enter your password ")
+# if username =="Arsii":
+#     if password =="1234":
+#         print("login successful")
+#     else:
+#         print("wrong password")    
+# else:
+#     print("wrong username")
+
+
+
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+operation=input("what operation do you want to do")
+if operation=="add":
+    print(num1+num2)
+elif operation== "minus":
+    print(num1-num2)
+elif operation=="division":
+    print(num1/num2)
+elif operation=="multiplication":
+    print(num1*num2)   
+else:
+    print("invalid operation")    
+    
+
+
+    
+    
