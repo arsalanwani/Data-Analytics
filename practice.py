df5f4a8aa2c5da7624cedfbe1002a103d09c0ec9
unknown <arsalanwani44@gmail.com>
2026-08-27 11:36:07 +0530

    filter

--- practice.py
+++ practice.py
@@ -1221,11 +1221,30 @@ students={
 # sqq=list(map(lambda x:x.upper(),sq))
 # print(sq)
 
-q=['arsalan', 'athar', 'wasiq', 'eeshaan', 'muzzamil']
-n=list(map(str.capitalize,q))
-print(n)
+# q=['arsalan', 'athar', 'wasiq', 'eeshaan', 'muzzamil']
+# n=list(map(str.capitalize,q))
+# print(n)
 
 
-listt=[1,2,3,4,5,-3,-4,-5,-6,-7]
-n=list(filter(lambda x:x<0,listt))
-print(n)
+# listt=[1,2,3,4,5,-3,-4,-5,-6,-7]
+# n=list(filter(lambda x:x<0,listt))
+# print(n)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sq=list(filter(lambda x:x%2==0,numbers))
+print(sq)
+
+numbers = [-5, 3, -2, 8, -1, 10, -7, 4]
+sq=list(filter(lambda x:x>0,numbers))
+print(sq)
+
+numbers = [2, 7, 4, 9, 1, 6, 3, 10]
+sq=list(filter(lambda x:x>5,numbers))
+print(sq)
+
+words = ["cat", "apple", "dog", "banana", "sun", "mango"]
+sq=list(filter(lambda x:len(x)>4,words))
+print(sq)
+
+numbers = [3, 7, 9, 12, 14, 15, 20, 21, 25]
+sq=list(filter(lambda x:x%3==0,numbers))
+print(sq)
